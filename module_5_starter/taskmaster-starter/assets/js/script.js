784b7819e3edfe7267fdaa91413de9646fc5e541
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-03-12 18:26:15 -0600

    customized/improved user experience

--- module_5_starter/taskmaster-starter/assets/js/script.js
+++ module_5_starter/taskmaster-starter/assets/js/script.js
@@ -76,16 +76,20 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
-    console.log(ui);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event, ui) {
-    console.log(ui);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log(event);
+    $(event.target).addClass("dropover-activate");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event) {
-    console.log(event);
+    $(event.target).removeClass("dropover-activate");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   update: function() {
     var tempArr = [];
@@ -152,7 +156,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -271,3 +275,9 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
